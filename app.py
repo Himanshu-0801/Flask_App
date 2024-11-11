35181dcc9140ca8e96c63bea890c51efe55768ef
Himanshu-0801 <himanshu7412yadav@gmail.com>
2024-11-11 21:38:35 +0530

    Request and inserting Ideas

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-
+from flask import Flask, request
+# the request is required to request the inserted body from the post
 app = Flask(__name__)
 
 
@@ -27,6 +27,36 @@ def get_all_ideas():
     #logic to fetch all the ideas
  return ideas
 
+
+'''
+// "Create a restful endpoint for creating a new idea"
+
+
+'''
+@app.post("/ideaapp/api/v1/ideas")
+def create_idea():
+    
+    
+    # logic to create new idea 
+    try:
+    # check the request of the new idea 
+        request_body = request.get_json( )
+
+     # check if ideas which is passed is already present or not 
+        if request_body["id"] and request_body["id"] in ideas:
+         return "the idea is already existing in the ideas ",400
+
+    #insert the passed idea in the idea dictionary
+        ideas[request_body["id"]] = request_body
+
+    # return the response that idea is submitted 
+        return "idea is entered successfully",201 
+    except KeyError:
+        return "id is missing",400
+    except:
+        return "Some internal server error",500    
+
+     
 
 
 
